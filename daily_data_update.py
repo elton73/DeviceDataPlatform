3df846cc969b705c893a9aeb1c28a231515ec018
Elton Lam <elton.lam@ahs.ca>
2023-04-19 13:59:00 -0600

    Daily updater now removes unused registration keys.

--- daily_data_update.py
+++ daily_data_update.py
@@ -5,6 +5,9 @@ from modules.send_email import check_last_sync
 from datetime import timedelta, date
 import pathlib
 import time
+from modules.mysql.setup import connect_to_database
+from modules.mysql.modify import purge_unused_keys
+from modules import LOGIN_DATABASE
 
 try:
     import httplib  # python < 3.0
@@ -22,9 +25,12 @@ def runschedule():
 
 if __name__ == '__main__':
     while True:
-        if time.localtime().tm_hour == 8 and time.localtime().tm_min == 45 and time.localtime().tm_sec == 0:
+        if time.localtime().tm_hour == 9 and time.localtime().tm_min == 0 and time.localtime().tm_sec == 0:
             print("Updating:")
             fitbit_users = runschedule()
             if fitbit_users:
                 check_last_sync(fitbit_users)
+            with connect_to_database(LOGIN_DATABASE) as login_db:
+                purge_unused_keys(login_db)
             time.sleep(60)
+
--- modules/mysql/modify.py
+++ modules/mysql/modify.py
@@ -147,6 +147,12 @@ def remove_health_data(user_id, db, device_type):
         return
     return False
 
+#Remove unused keys
+def purge_unused_keys(db):
+    mycursor = db.cursor()
+    mycursor.execute(f"DELETE FROM registration_keys WHERE email is NULL")
+    db.commit()
+
 # Input (userid, device_type, auth_token, refresh_token, and expires by) data into mysql
 def export_device_to_auth_info(auth_info, db):
     userid = auth_info['user_id']
